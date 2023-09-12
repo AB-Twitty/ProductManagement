d3a7aff391d8a2bb932a1230de611b8c503386a8
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2023-09-12 23:28:05 +0300

    override the save method
    override the save method in the context to update the information of the modification whenever a new save is called, and if the entity state is added then it sets the Id to a guid to be unique

--- ProductManagement.Persistence/Context/ProductDbContext.cs
+++ ProductManagement.Persistence/Context/ProductDbContext.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Domain.Bases;
 using ProductManagement.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ProductManagement.Persistence.Context
 {
@@ -8,6 +12,23 @@ namespace ProductManagement.Persistence.Context
 		public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
 		{
 
+		}
+
+		public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			foreach (var entity in ChangeTracker.Entries<BaseEntity>())
+			{
+				entity.Entity.UpdatedBy = "Admin";
+				entity.Entity.LastUpdatedDate = DateTime.UtcNow;
+
+				if (entity.State == EntityState.Added)
+				{
+					entity.Entity.Id = Guid.NewGuid().ToString();
+				}
+			}
+
+
+			return base.SaveChangesAsync(cancellationToken);
 		}
 
 		public virtual DbSet<Category> Categories { get; set; }
